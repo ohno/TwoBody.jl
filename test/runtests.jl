48655f3af9ccdc7595854ac53fb51500b2ef363d
Shuhei Ohno <ohno.github@gmail.com>
2024-12-13 12:02:05 +0900

    Update runtests.jl for new tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,7 +15,17 @@ using QuadGK
   )
   res = solve(H, BS)
   # Thijssen(2007)
-  @test isapprox(res.E[1], -0.499278, rtol=1e-6)
-  # normalization
-  @test quadgk(r -> 4π*r^2*res.ψ[1](r)^2, 0, Inf, rtol=1e-12, maxevals=10^7)[1] ≈ 1
+  @test isapprox(-0.499278, res.E[1], atol=1e-6)
+  # ψ
+  for i in keys(res.E)
+    @test isapprox(1, 4π *quadgk(r -> r^2 * abs(res.ψ[i](r))^2, 0, Inf, rtol=1e-12, maxevals=10^7)[1], atol=1e-6)
+  end
+  # <ψₙ|ψₙ> = cₙ' * S * cₙ = 1
+  for i in keys(res.E)
+    @test isapprox(1, res.C[:,i]' * res.S * res.C[:,i], atol=1e-6)
+  end
+  # |<ψₙ|H|ψₙ> - E| = |cₙ' * H * cₙ - E| = 0
+  for i in keys(res.E)
+    @test isapprox(0, abs((res.C[:,i]' * res.H * res.C[:,i]) - res.E[i]), atol=1e-6)
+  end
 end
