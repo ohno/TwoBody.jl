6b880fcd1ad351316351f0d620743f9b1f38f152
Shuhei Ohno <ohno.github@gmail.com>
2025-05-05 05:35:16 +0900

    Update docstrings in FDM.jl

--- src/FDM.jl
+++ src/FDM.jl
@@ -10,14 +10,14 @@ import Printf
 @doc raw"""
 `FiniteDifferenceMethod(Δr=0.1, rₘₐₓ=50.0, R=Δr:Δr:rₘₐₓ, l=0, direction=:c, solver=:LinearAlgebra)`
 
-| Arguments | Symbol |
-| :-------- | :----- |
-| `Δr::Real         ` | Radial grid spacing. A uniform grid spacing is used, ``r_{i+1} = r_{i} + \Delta r``. |
-| `rₘₐₓ::Real       ` | This value is not directly used in the calculation, but it is used to determine the `R`. |
-| `R::StepRangeLen  ` | Radial grid |
-| `l::Int           ` | Angular momentum quantum number |
-| `direction::Symbol` | `:c` for central, :f for forward, `:b` for backward |
-| `solver::Symbol   ` | `:LinearAlgebra` or `:ArnoldiMethod` |
+| Arguments | Default | Description |
+| :-------- | :------ | :---------- |
+| `Δr::Real`          | `0.1`            | Radial grid spacing. A uniform grid spacing is used, ``r_{i+1} = r_{i} + \Delta r``. |
+| `rₘₐₓ::Real`        | `50.0`           | The maximum value of the radial grid. This value is not directly used in the calculation, but it is used to determine the `R`. |
+| `R::StepRangeLen`   | `Δr:Δr:rₘₐₓ`     | Radial grid. The origin must be excluded from the grid to avoid divergence of the Coulomb potential and the centrifugal potential at the origin. |
+| `l::Int`            | `0`              | Angular momentum quantum number. This is a positive integer, ``0 \leq l``. |
+| `direction::Symbol` | `:c`             | The direction of the finite difference, `:c` for central, :f for forward, `:b` for backward. |
+| `solver::Symbol`    | `:LinearAlgebra` | The solver for eigenvalue problem, `:LinearAlgebra` or `:ArnoldiMethod`. |
 """
 struct FiniteDifferenceMethod
   Δr::Real
@@ -174,14 +174,14 @@ is written as
     \left(\begin{array}{ccccccc}
        0 &  1 &  0 & \ldots \\
       -1 &  0 &  1 & \ldots \\
-       0 & -1 & -1 & \ldots \\
+       0 & -1 &  0 & \ldots \\
       \vdots & \vdots & \vdots & \ddots \\
     \end{array}\right)
     - l(l+1)
     \left(\begin{array}{ccccccc}
-       {r_1}^2 & 0 & 0 & \ldots \\
-       0 & {r_2}^2 & 0 & \ldots \\
-       0 & 0 & {r_3}^2 & \ldots \\
+       1/{r_1}^2 & 0 & 0 & \ldots \\
+       0 & 1/{r_2}^2 & 0 & \ldots \\
+       0 & 0 & 1/{r_3}^2 & \ldots \\
       \vdots & \vdots & \vdots & \ddots \\
     \end{array}\right)
   \right].
